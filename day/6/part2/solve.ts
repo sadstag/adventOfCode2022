d55fa5b6bc7f55848de87dfa6e375c5bde7140b2
sadstag <fred.sarron@gmail.com>
2022-12-06 13:17:15 +0100

    day 6

--- day/6/part2/solve.ts
+++ day/6/part2/solve.ts
@@ -2,12 +2,12 @@ import { Solver } from "common";
 import { Output, Input } from "./types.ts";
 
 const hasDoublon = (s: string): boolean => {
-  return new Set([...s]).size !== 4;
+  return new Set([...s]).size !== 14;
 };
 
 export const solve: Solver<Input, Output> = (input: Input) => {
-  for (let i = 4; i < input.length; i++) {
-    const candidate = input.slice(i - 4, i);
+  for (let i = 14; i < input.length; i++) {
+    const candidate = input.slice(i - 14, i);
     if (hasDoublon(candidate)) continue;
     return i;
   }
